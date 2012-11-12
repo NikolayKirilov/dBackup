8c64a926cdeb08a5eac50ea0438960f8670f5490
Nikolay Kirilov <kirilovnv@gmail.com>
2012-11-12 23:19:27 +0200

    Created abstract classes to represent some basic features.

---
+++ src/info/dbackup/client/Device.java
@@ -0,0 +1,7 @@
+package info.dbackup.client;
+
+public class Device {// May be an interface
+	// The idea is to represent client devices master/slaves to be backed up or
+	// used for backup data storage.
+
+}
---
+++ src/info/dbackup/data/AbstractFile.java
@@ -0,0 +1,8 @@
+package info.dbackup.data;
+
+public class AbstractFile {// May ba an interface?
+
+	// To be used to represent all data which can be backed up.
+	// For example files (images,mp3,documents etc.)
+
+}
