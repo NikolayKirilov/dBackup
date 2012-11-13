d27515848eafa07268021aecbf14af9736d4b2f1
NikolayKirilov <kirilovnv@gmail.com>
2012-11-13 10:54:22 +0200

    Update src/info/dbackup/client/Device.java

--- src/info/dbackup/client/Device.java
+++ src/info/dbackup/client/Device.java
@@ -3,5 +3,7 @@ package info.dbackup.client;
 public class Device {// May be an interface
 	// The idea is to represent client devices master/slaves to be backed up or
 	// used for backup data storage.
+	
+	//TODO reove this comment (test of edittiong file from guthub web interface.)
 
 }
